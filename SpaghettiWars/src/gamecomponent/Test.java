834012fece7e225e4ab21639f1442b97933dcb33
Xiaoming94 <henyan94@gmail.com>
2014-03-21 09:42:27 +0100

    Cleaned up my tests (sry, guys)

--- SpaghettiWars/src/gamecomponent/Test.java
+++
@@ -1,9 +0,0 @@
-package gamecomponent;
-
-public class Test{
-	/*If this works, It will be awesome*/
-	public static void printTest(){
-		System.out.println("This worked too");
-        new Test2();
-	}
-}
--- SpaghettiWars/src/gamecomponent/Test2.java
+++
@@ -1,9 +0,0 @@
-package gamecomponent;
-
-//Another test, hopefully this works
-
-public class Test2{
-	public Test2(){
-		System.out.println("Hopefully this works");
-	}
-}
