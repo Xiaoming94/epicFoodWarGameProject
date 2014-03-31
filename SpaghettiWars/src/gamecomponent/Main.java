54c06e4d48ab85dbb0127fc329c1fcd4e087e320
JimmyMalmer <jimmy.eliasson@hotmail.se>
2014-03-31 12:58:33 +0200

    Setup the gdx libraries

--- SpaghettiWars/src/gamecomponent/Main.java
+++ SpaghettiWars/src/gamecomponent/Main.java
@@ -1,9 +1,49 @@
 package gamecomponent;
 
-public class Main {
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
+
+public class Main implements ApplicationListener{
 
 	public static void main(String[] args){
 		ClientModel model = new ClientModel();
+		new LwjglApplication(new Main(), "Game", 480, 320);
+	}
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
 		
 	}
 }
