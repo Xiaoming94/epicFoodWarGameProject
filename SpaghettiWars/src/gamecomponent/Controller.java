b3fa25b474ee0997a1e03e0864259b85d6823da2
Henry Yang <henryy@ed5355-09.ed5355.studat.a.1.hb.cdal.chalmers.se>
2014-04-04 12:14:12 +0200

    Enhanced some code

--- SpaghettiWars/src/gamecomponent/Controller.java
+++ SpaghettiWars/src/gamecomponent/Controller.java
@@ -1,5 +1,7 @@
 package gamecomponent;
 
+import utilities.GameInputHandler;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
@@ -8,19 +10,17 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import entities.Player;
 import entities.Projectile;
 
-public class Controller implements Runnable, InputProcessor {
+public class Controller implements Runnable {
 
 	Model model;
-	private boolean up, down, left, right;
 	
-	private Direction currentDirection;
-
-	public enum Direction{
+	/*public enum Direction{
 		NORTH,SOUTH,WEST,EAST,NORTHWEST,NORTHEAST,SOUTHWEST,SOUTHEAST,STAY
-	}
+	}*/
+	
 	public Controller(Model m) {
 		model = m;
-		Gdx.input.setInputProcessor(this);
+		Gdx.input.setInputProcessor(new GameInputHandler(model));
 	}
 	
 
@@ -36,7 +36,6 @@ public class Controller implements Runnable, InputProcessor {
 		
 		model.addEntity(new Player("Sir derp", 50, 100, new Sprite(model.getTextureByName("dummylogo.png")), 0));
 
-		model.getPlayer().setVector(0.5, 1);
 		while (true) {
 			model.getPlayer().move();
 
@@ -49,115 +48,5 @@ public class Controller implements Runnable, InputProcessor {
 		}
 
 	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		System.out.println(keycode);
-
-		switch (keycode) {
-		case (Keys.W):
-			up = true;
-			break;
-		case (Keys.S):
-			down = true;
-			break;
-		case (Keys.A):
-			left = true;
-			break;
-		case (Keys.D):
-			right = true;
-			break;
-		}
-		getDirection();
-		model.getPlayer().setPlayerDirection(currentDirection);
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-
-		switch (keycode) {
-		case (Keys.W):
-			up = false;
-			break;
-		case (Keys.S):
-			down = false;
-			break;
-		case (Keys.A):
-			left = false;
-			break;
-		case (Keys.D):
-			right = false;
-			break;
-		}
-		getDirection();
-		model.getPlayer().setPlayerDirection(currentDirection);
-		
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	public void getDirection(){
-		currentDirection = Direction.STAY;
-
-		if(up && !down){
-			if(left && !right){
-				currentDirection = Direction.NORTHWEST;
-			}else if(right && !left){
-				currentDirection = Direction.NORTHEAST;
-			}else{
-				currentDirection = Direction.NORTH;
-			}
-		}
-		else if(down && !up){
-			if(left && !right){
-				currentDirection = Direction.SOUTHWEST;
-			}else if(right && !left){
-				currentDirection = Direction.SOUTHEAST;
-			}else{
-				currentDirection = Direction.SOUTH;
-			}
-		}else if(right && !left){
-			currentDirection = Direction.EAST;
-		}else if(left && !right){
-			currentDirection = Direction.WEST;
-		}
-		
-		//return currentDirection;
-		
-	}
+	
 }
--- SpaghettiWars/src/gamecomponent/Model.java
+++ SpaghettiWars/src/gamecomponent/Model.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import utilities.NameTexture;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
@@ -16,6 +17,7 @@ import entities.Player;
 
 public class Model {
 	
+	private boolean upKeyPressed,downKeyPressed,leftKeyPressed,rightKeyPressed;
 	ArrayList<Entity> entities;
 	Player player;
 	
@@ -53,5 +55,103 @@ public class Model {
 				return e;
 		return null;
 	}
+	
+	public void checkPressedKey (int keyCode){
+		switch (keyCode){
+		case Keys.W: 
+			upKeyPressed = true;
+			break;
+		case Keys.A:
+			leftKeyPressed = true;
+			break;
+		case Keys.S:
+			downKeyPressed = true;
+			break;
+		case Keys.D:
+			rightKeyPressed = true;
+			break;
+		default:
+			return;
+		}
+		
+		updatePlayerMovingDirection();
+		
+	}
+	
+	public void checkReleasedKey (int keyCode){
+		switch (keyCode){
+		case Keys.W: 
+			upKeyPressed = false;
+			break;
+		case Keys.A:
+			leftKeyPressed = false;
+			break;
+		case Keys.S:
+			downKeyPressed = false;
+			break;
+		case Keys.D:
+			rightKeyPressed = false;
+			break;
+		default:
+			return;
+		}
+		
+		updatePlayerMovingDirection();
+	}
+
+	private void updatePlayerMovingDirection() {
+		if(upKeyPressed && !downKeyPressed){
+			if(leftKeyPressed && !rightKeyPressed){
+				player.getVector().setVectorByDegree(player.getSpeed(), 135);
+			}else if(rightKeyPressed && !leftKeyPressed){
+				player.getVector().setVectorByDegree(player.getSpeed(), 45);
+			}else{
+				player.setVector(0, 1*player.getSpeed());
+			}
+		}
+		else if(downKeyPressed && !upKeyPressed){
+			if(leftKeyPressed && !rightKeyPressed){
+				player.getVector().setVectorByDegree(player.getSpeed(), 225);
+			}else if(rightKeyPressed && !leftKeyPressed){
+				player.getVector().setVectorByDegree(player.getSpeed(), 315);
+			}else{
+				player.setVector(0, (-1)*player.getSpeed());
+			}
+		}else if(rightKeyPressed && !leftKeyPressed){
+			player.setVector(player.getSpeed(), 0);
+		}else if(leftKeyPressed && !rightKeyPressed){
+			player.setVector((-1)*player.getSpeed(), 0);
+		}else{
+			player.setVector(0, 0);
+		}
+	}
+	
+//	public void getDirection(){
+//		currentDirection = Direction.STAY;
+//
+//		if(up && !down){
+//			if(left && !right){
+//				currentDirection = Direction.NORTHWEST;
+//			}else if(right && !left){
+//				currentDirection = Direction.NORTHEAST;
+//			}else{
+//				currentDirection = Direction.NORTH;
+//			}
+//		}
+//		else if(down && !up){
+//			if(left && !right){
+//				currentDirection = Direction.SOUTHWEST;
+//			}else if(right && !left){
+//				currentDirection = Direction.SOUTHEAST;
+//			}else{
+//				currentDirection = Direction.SOUTH;
+//			}
+//		}else if(right && !left){
+//			currentDirection = Direction.EAST;
+//		}else if(left && !right){
+//			currentDirection = Direction.WEST;
+//		}
+		
+		//return currentDirection;
 
 }
