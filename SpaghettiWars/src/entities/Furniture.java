0f1be845cc78c3a9cdfdd8d6c6937de83a658a15
louisejohannesson <koppajojmen@hotmail.com>
2014-04-08 17:37:02 +0200

    added furniture class

---
+++ SpaghettiWars/src/entities/Furniture.java
@@ -0,0 +1,9 @@
+package entities;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Furniture extends Obstacle{
+	public Furniture(double x, double y, Sprite sprite){
+		super(x, y, sprite, false);
+	}
+}
