6d03c49e47f1a157d966aacaccf2799908631bd4
Henry Yang <henyan94@gmail.com>
2014-04-09 13:35:11 +0200

    Enhanced menu

--- SpaghettiWars/src/gamecomponent/MenuScreen.java
+++ SpaghettiWars/src/gamecomponent/MenuScreen.java
@@ -2,9 +2,17 @@ package gamecomponent;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+//import java.awt.*;
 
 /**
  * Created by xiaoming on 09/04/14.
@@ -25,16 +33,52 @@ public class MenuScreen implements Screen{
         Gdx.input.setInputProcessor(stage);
 
         skin = new Skin();
+
+        Pixmap pixmap = new Pixmap(100,100, Pixmap.Format.RGB888);
+        pixmap.setColor(Color.GREEN);
+        pixmap.fill();
+
+        skin.add("white", new Texture(pixmap));
+
+        BitmapFont bfont = new BitmapFont();
+        bfont.scale(1);
+        skin.add("default",bfont);
+
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+
+        textButtonStyle.font = skin.getFont("default");
+
+        skin.add("default", textButtonStyle);
+
+        final TextButton textButton = new TextButton("PLAY",textButtonStyle);
+        textButton.setPosition(200,200);
+        stage.addActor(textButton);
+        stage.addActor(textButton);
+        stage.addActor(textButton);
     }
 
     @Override
-    public void render(float delta) {
-
+    public void render (float delta) {
+        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+        Table.drawDebug(stage);
     }
 
     @Override
-    public void resize(int width, int height) {
+    public void resize (int width, int height) {
+        stage.setViewport(width, height, false);
+    }
 
+    @Override
+    public void dispose () {
+        stage.dispose();
+        skin.dispose();
     }
 
     @Override
@@ -57,8 +101,4 @@ public class MenuScreen implements Screen{
 
     }
 
-    @Override
-    public void dispose() {
-
-    }
 }
