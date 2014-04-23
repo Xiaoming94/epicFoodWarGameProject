a20c5dc67243ea300d9943ef68d70595075a67e5
Henry Yang <henyan94@gmail.com>
2014-04-23 23:14:25 +0200

    Start coding Menu Screen

---
+++ SpaghettiWars/src/gamecomponent/views/MenuScreen.java
@@ -0,0 +1,21 @@
+package gamecomponent.views;
+
+import java.beans.PropertyChangeSupport;
+
+/**
+ * Created by xiaoming on 21/04/14.
+ */
+public class MenuScreen implements GameScreen {
+
+
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void setToCorrectInputProcessor() {
+
+    }
+}
