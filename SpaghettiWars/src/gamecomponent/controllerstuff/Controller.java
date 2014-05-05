9c6a9e86e1948e6d17a9cc1dc0e49904bd17bda9
louisejohannesson <koppajojmen@hotmail.com>
2014-05-05 13:12:37 +0200

    moving around meatball detection

--- SpaghettiWars/src/gamecomponent/controllerstuff/Controller.java
+++ SpaghettiWars/src/gamecomponent/controllerstuff/Controller.java
@@ -26,6 +26,8 @@ public class Controller implements Runnable {
 		model = m;
 		utilobject = uo;
 		
+		utilobject.addModel(model);//ny
+		
 		System.out.println("controller skapas");
 	}
 
@@ -106,31 +108,12 @@ public class Controller implements Runnable {
 						}
 					}
 					
+					utilobject.run();
 					
-//					//Collision detection for meatballs won't work if meatball
-//					//hits the thrower
-//					//what the fuck is going on with IDs?
-//					Collection <Player> opponents = model.getOtherPlayers().values();
-//					for(Player opp: opponents){
-//						System.out.println("model player ID: " + model.getPlayer().getID());
-//						System.out.println("meatball ID: " + e.getID());
-//						System.out.println("player ID: " + opp.getID());
-//						System.out.println(opponents.size());
-//						if(e.getSprite().getBoundingRectangle().overlaps(opp.getSprite().getBoundingRectangle()) && e.getID()/1000 != opp.getID()/1000){
-//							e.kill();
-//							killProjectileList.add(e);
-//							opp.gainWeight(e.getDamage());
-//							
-//						}
-//					}
-//					if(model.getPlayer().getSprite().getBoundingRectangle().overlaps(e.getSprite().getBoundingRectangle()) && e.getID()/1000!= model.getPlayer().getID()/1000){
-//						e.kill();
-//						killProjectileList.add(e);
-//						model.getPlayer().gainWeight(e.getDamage());
-//					}
-					//end of meatball detection
 					
 				}
+				
+				
 
 				//pizza
 				if (e instanceof entities.Pizza) {
--- SpaghettiWars/src/gamecomponent/controllerstuff/IControllerUtil.java
+++ SpaghettiWars/src/gamecomponent/controllerstuff/IControllerUtil.java
@@ -1,6 +1,12 @@
 package gamecomponent.controllerstuff;
 
-public interface IControllerUtil {
+import gamecomponent.Model;
 
+public interface IControllerUtil {
+	
+
+	//public void run();
 	public void run();
+	
+	public void addModel(Model m);//ny
 }
